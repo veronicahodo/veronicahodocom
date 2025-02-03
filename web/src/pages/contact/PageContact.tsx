6d35bc340c0d3a706add656a347b11d4e12d0e1f
Veronica Hodo <veronica@hodo.llc>
2025-02-03 14:49:01 -0700

    Added Github to contacts

--- web/src/pages/contact/PageContact.tsx
+++ web/src/pages/contact/PageContact.tsx
@@ -12,18 +12,24 @@ const PageContact = () => {
                     </a>
                 </p>
                 <p>
-                    Facebook:{" "}
-                    <a
-                        target="_blank"
-                        href="https://facebook.com/veronicahodo/"
-                    >
-                        https://facebook.com/veronicahodo/
+                    Github:{" "}
+                    <a target="_blank" href="https://github.com/veronicahodo">
+                        https://github.com/veronicahodo
                     </a>
                 </p>
                 <p>
                     BlueSky:{" "}
                     <a target="_blank" href="https://veronicahodo.bsky.social/">
                         https://veronicahodo.bsky.social/
+                    </a>
+                </p>
+                <p>
+                    Facebook:{" "}
+                    <a
+                        target="_blank"
+                        href="https://facebook.com/veronicahodo/"
+                    >
+                        https://facebook.com/veronicahodo/
                     </a>
                 </p>
                 <p>X can eat a bag of dicks. Fuck Elon.</p>
